b30100c72005c812dae7a643a07c7527c55d64a2
mrnando503 <131102186+mrnando503@users.noreply.github.com>
2023-04-22 14:14:03 -0700

    Add files via upload

---
+++ New-FrontEnd-Labs/week01/Week01VariablesAndOperations.js
@@ -0,0 +1,136 @@
+/*
+  Copyright (c) 2023 Promineo Tech
+  Author:  Promineo Tech Academic Team
+  Subject:  Variables and Operations Lab
+  FE 01 Lab  
+*/
+
+/** Key Terms:
+ *  JavaScript
+ *  GUI (Graphic User Interface)
+ *  CLI (Command Line Interface)
+ *  Variable
+ *  Data Types:
+ *   - String
+ *   - Number
+ *   - Boolean
+ *   - Array
+ *   - Undefined
+ *   - Object
+ *   - BigInt
+ *   - Null
+ *   - Symbol
+ *  Method
+ *  Git
+ *  Source Control
+ *  Operators
+ *  console.log()
+ * 
+*/
+
+/* ----------------------------------------------------- */
+// Please do not alter the existing code unless instructed to do so. 
+// Read the comments and add your code where it is specified for each question. 
+/* ----------------------------------------------------- */
+
+/*-------------------------------------------------------*/
+
+console.log(`--------------------------
+Question 1. Creating & 'console.log()'ing variables \n`);
+
+
+
+/**
+ * Instruction: Create a variable for each step, then print that variable 
+ *              to the console in the correct section.
+ *
+ * Step 1: Create a variable to hold the quantity of available seats left on a flight.
+ * Step 2: Create a variable to hold the cost of groceries at checkout
+ * Step 3: Create a variable to hold a person's middle initial
+ * Step 4: Is it Hot Outside?
+ *         Create a variable to hold a boolean, true if it's hot outside, false if it's not hot outside.
+ * Step 5: Create a variable to hold a customer's first name.
+ * Step 6: Create a variable to hold a street address.
+ * ↓ YOUR CODE HERE ↓ */
+
+
+console.log("Step 1:  Number of Seats on a Plane");
+var seatsAvailbleInFlight = 7;
+console.log("the Number of Available seats is " + seatsAvailbleInFlight);
+
+console.log("Step 2:  Cost of Groceries");
+var thecostofgroceriesis = 40.00;
+console.log("The Cost of Groceries is " + thecostofgroceriesis);
+
+console.log("Step 3:  Middle Initial");
+var middleinitialis = "F";
+console.log("My middle inital is " + middleinitialis);
+
+
+console.log("Step 4:  Is it Hot Outside?");
+var isitHotOutside = false;
+console.log("the Weather Outside is not Hot: " + isitHotOutside);
+
+
+console.log("Step 5:  First Name");
+var firstName = "Edwin"
+console.log("my first name is: " + firstName);
+
+console.log("Step 6:  Street Address");
+var streetAddress = "3649 Holden St"
+console.log("Streen Address is: " + streetAddress);
+
+/*-------------------------------------------------------*/
+
+console.log(`--------------------------
+Question 2. Creating & 'console.log()'ing variables \n`);
+ 
+
+/** 
+ * Instruction: Use the variables we just created to solve the problems below.
+ *              Print your answers to the console.
+ * 
+ *
+ * */
+
+    console.log('Remove Plane Seats:')
+//	A customer booked 2 plane seats. Remove 2 seats from the available seats variable
+//  ↓ YOUR CODE HERE ↓
+console.log("A customer Purchased two seats " + (seatsAvailbleInFlight - 2) + "seats left ");
+
+    console.log('Buy a Candy Bar:')
+//	Impulse candy bar purchase! Add 2.15 to the grocery total
+//  ↓ YOUR CODE HERE ↓
+
+console.log("I have a extra candy bar in my grocery cart. Total of groceries is now: " 
++ (thecostofgroceriesis + 2.15));
+
+    console.log('Fix a Birth Certificate:')
+//	A birth certificate was printed incorrectly, change the middle initial to something else
+//  ↓ YOUR CODE HERE ↓
+
+middleinitialis = "D"
+console.log("Changing middle initial to correct letter. The Correct letter is: " + middleinitialis);
+
+    console.log('Changing Seasons:')
+//	The season has changed, update the hot outside variable	to be opposite of what it was
+//  ↓ YOUR CODE HERE ↓
+    isitHotOutside = "True"
+    console.log("The weather has changed. " + isitHotOutside);
+
+    console.log('Full Name:')
+//	Create a new variable called full name using the customer's
+//  first name, the middle initial, and a last name of your choice
+//  ↓ YOUR CODE HERE ↓
+ 
+var fullName = firstName + " " + middleinitialis + " " + "Harris"
+console.log("Customers Full name is: " + fullName);
+
+    console.log('Introduce a Customer:')
+//	Print a line to the console that introduces the customer 
+//	including the name, and saying that they live at the address variable
+//  ↓ YOUR CODE HERE ↓
+
+console.log ("The customers name is: " + fullName);
+
+    console.log('-----------Finished------------')
